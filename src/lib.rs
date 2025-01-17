2936986b20cd66ce858b16c7052f7023789f12b6
Will Morris <morris.william.work@gmail.com>
2025-01-17 12:02:02 -0500

    Add early wasm json greeting

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 mod utils;
 
+use walc_model::Token;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
@@ -10,4 +11,11 @@ extern {
 #[wasm_bindgen]
 pub fn greet(name: &str) {
     alert(&format!("Hello, #{name}!"));
+}
+
+#[wasm_bindgen]
+/// Given a tree in JSON format.
+pub fn calc(tree: &str) {
+    let ast: Token = serde_json::from_str(tree).unwrap();
+    greet(&format!("{:?}", ast.evaluate()));
 }
