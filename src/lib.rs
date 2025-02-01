b9751e31b65a7997aecc98113d63b2e48379d470
Will Morris <morris.william.work@gmail.com>
2025-02-01 09:27:28 -0500

    Switch to newer walc version

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 mod utils;
 
-use walc_model::ast::token::Token;
+use walc_model::ast::ast::ASTNode;
 use walc_model::ast::treewalk_interpreter::interpret;
 use wasm_bindgen::prelude::*;
 
@@ -18,7 +18,7 @@ pub fn greet(name: &str) {
 /// Given a tree in JSON format, evaluate it and return a string representation of the result.
 /// First character will be E or O, representing Error or OK.
 pub fn calc(tree: &str) -> String {
-    let translation = serde_json::from_str::<Token>(tree);
+    let translation = serde_json::from_str::<ASTNode>(tree);
     let ast = match translation {
         Ok(ast) => ast,
         Err(message) => return format!("E{:?}", message.to_string()),
