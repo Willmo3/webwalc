5e33ee89fc5928a762d9f4c18d846d3cab0304f6
Will Morris <morris.william.work@gmail.com>
2025-02-05 15:44:23 -0500

    Update Rust bindings

--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,14 @@
 mod utils;
 
-use walc_model::ast::ast::ASTNode;
-use walc_model::ast::treewalk_interpreter::interpret;
+use walc_model::interpret;
 use wasm_bindgen::prelude::*;
-
-#[wasm_bindgen]
-extern {
-    fn alert(s: &str);
-}
-
-#[wasm_bindgen]
-pub fn greet(name: &str) {
-    alert(&format!("Hello, #{name}!"));
-}
 
 #[wasm_bindgen]
 /// Given a tree in JSON format, evaluate it and return a string representation of the result.
 /// First character will be E or O, representing Error or OK.
-pub fn calc(tree: &str) -> String {
-    let translation = serde_json::from_str::<ASTNode>(tree);
-    let ast = match translation {
-        Ok(ast) => ast,
-        Err(message) => return format!("E{:?}", message.to_string()),
-    };
-    match interpret(&ast) {
-        Ok(result) => format!("O{:?}", result),
-        Err(message) => format!("E{:?}", message),
+pub fn execute(source: &str) -> String {
+    match interpret(source) {
+        Ok(result) => format!("O{}", result),
+        Err(message) => format!("E{}", message)
     }
 }
