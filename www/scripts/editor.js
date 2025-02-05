0c709d3f5a739e64227b538e28cd012f8ab3dab9
Will Morris <morris.william.work@gmail.com>
2025-02-05 15:44:01 -0500

    Switch to new Rust-only frontend

--- www/scripts/editor.js
+++ www/scripts/editor.js
@@ -1,8 +1,4 @@
-import { lex } from '../lib/webwalc.ts/src/frontend/lexer.js';
-import { parse } from '../lib/webwalc.ts/src/frontend/parser.js';
-import { WebwalcJsonVisitor } from '../lib/webwalc.ts/src/ast/webwalcJsonVisitor.js';
-import { postTraverseAST } from '../lib/webwalc.ts/src/ast/ast.js';
-import init, { calc } from '../pkg/webwalc.js';
+import init, { execute } from '../pkg/webwalc.js';
 
 // Ensure WebAssembly initialization complete before continuing.
 await init();
@@ -19,27 +15,7 @@ editor.setOptions({
 });
 
 document.getElementById('submitButton').addEventListener('click', () => {
-    const input = editor.getValue();
-
-    // Error checking from frontend follows looser API.
-    const lexemes = lex(input);
-    if (lexemes.hasOwnProperty("errorMessage")) {
-        document.getElementById('outputBox').style.color = 'red';
-        document.getElementById('outputBox').innerText = lexemes.errorMessage;
-        return;
-    }
-
-    const tree = parse(lexemes);
-    if (tree.hasOwnProperty("errorMessage")) {
-        document.getElementById('outputBox').style.color = 'red';
-        document.getElementById('outputBox').innerText = tree.errorMessage;
-        return;
-    }
-
-    const visitor = new WebwalcJsonVisitor();
-    postTraverseAST(tree, visitor);
-
-    const output = calc(visitor.result());
+    const output = execute(editor.getValue());
     // Returning result across WebAssembly, so indicating error or OK by first character.
     if (output[0] === "E") {
         document.getElementById('outputBox').style.color = 'red';
